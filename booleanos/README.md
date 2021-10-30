3b75d0ca95ef66e71b0e85fddf4f3b1a039f910a
johan2613 <79009497+johan2613@users.noreply.github.com>
2021-10-31 02:43:40 +0700

    Add files via upload

---
+++ booleanos/README.md
@@ -0,0 +1,1 @@
+# booleanos-y-condicionales
---
+++ booleanos/js/index.js
@@ -0,0 +1,13 @@
+alert("hola mundo");
+console.log(2 > 4); //false
+console.log(2 == 2); //false
+console.log("42" == 42); //true
+console.log("42" === 42); //false
+let resultado = 5 === 4;
+console.log(resultado); //false
+console.log(2 == 3);
+
+let prueba = 5 != 5;
+console.log(prueba);
+let prueba2 = "5" !== 5;
+console.log(prueba2);
